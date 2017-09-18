d44f861cc557e98e7bc40b6cf04ca2495fa84436
Lenovo <Lenovo@172.29.80.64>
2017-09-18 11:23:14 -0400

    Initial Commit Bonjour

--- src/bonjournom/BonjourNom.java
+++ src/bonjournom/BonjourNom.java
@@ -16,7 +16,7 @@ public class BonjourNom {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Bonjour " + args[0]+ " " + args[1]);
+        System.out.println("Bonjour ");
     }
     
 }
